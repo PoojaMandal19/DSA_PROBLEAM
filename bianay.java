55a20b92c80304282ca5eccb4ebe3d71bf4109e3
PoojaMandal19 <poojamandal1905@gmail.com>
2026-08-05 19:49:43 +0530

    update

--- bianay.java
+++ bianay.java
@@ -1,5 +1,40 @@
 public class bianay {
+    public static int binaryserch(int arrey[],int key) {
+        int left = 0;
+        int right = arrey.length-1;
+      
+
+       
+
+        while (left<=right) {
+              int mid = (left+right)/2;
+              if (arrey[mid] == key) 
+                return mid;
+            else if (arrey[mid]<key) {
+                left = mid +1;
+                else{ 
+                  
+                    right = mid -1;
+                }
+            }
+              return -1;
+            
+        }
+      
+                
+            
+    
+           
+                
+            
+        }
+
+    
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int arrrey[] = {10,20,30,40,50,60,70};
+        int key = 40;
+        System.out.println(binaryserch(arrrey, key));
+
+
     }
 }
